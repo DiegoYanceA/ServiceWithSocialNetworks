3c4831ef1385ab4af4aa220df35079ac7359e9eb
Diego Yance Arqque <dyance@santillana.com>
2021-04-11 00:45:22 -0500

    asdsadddsaasd

--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ const cors = require('cors');
 const mode = process.env.MODE || "DEVELOPMENT";
 
 // ignore request for FavIcon. so there is no error in browser
-const ignoreFavicon = (req, res, next) => {
-    if (req.originalUrl.includes('favicon.ico')) {
-        res.status(204).end();
-    }
-    next();
-};
+// const ignoreFavicon = (req, res, next) => {
+//     if (req.originalUrl.includes('favicon.ico')) {
+//         res.status(204).end();
+//     }
+//     next();
+// };
 
 // fn to create express server
 const create = async () => {
@@ -20,18 +20,18 @@ const create = async () => {
     const app = express();
 
     // configure nonFeature
-    app.use(ignoreFavicon);
+    // app.use(ignoreFavicon);
     
     //json
     app.use(express.json());
     app.use(express.urlencoded({extended: true}))
+    app.use(cors());
     
     if(mode == "PRODUCTION"){
-        app.use(express.static(__dirname + '/../public/'));
         app.use(history());
+        app.use(express.static(__dirname + '/../public/'));
+        
     }
-
-    app.use(cors());
 
     // root route - serve static file
     app.get('/', (req, res) => {
