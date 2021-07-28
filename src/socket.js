9bb90c21b0f796db9033d57e11255b2ceeacdf30
Diego Yance Arqque <dyance@santillana.com>
2021-07-28 15:32:54 -0500

    asd

--- src/socket.js
+++ src/socket.js
@@ -12,7 +12,7 @@ module.exports = (http) => {
 
   var messageAux = "";
   var emoji = "https://static-cdn.jtvnw.net/emoticons/v2/307595784/default/light/1.0";
-  var background = "https://yoguidrogui.com/wp-content/uploads/2020/01/guia-setup-gaming-gamer-750x375.jpg";
+  var background = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6169b41c-74fe-4235-9815-1e8d4dc87406/d3hgxl4-a6d413df-c24f-4077-b1d2-8765659ef170.png/v1/fill/w_900,h_800,strp/png_fondo_blanco_by_camilhitha124_d3hgxl4-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvNjE2OWI0MWMtNzRmZS00MjM1LTk4MTUtMWU4ZDRkYzg3NDA2XC9kM2hneGw0LWE2ZDQxM2RmLWMyNGYtNDA3Ny1iMWQyLTg3NjU2NTllZjE3MC5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.cqTa97iqaSe0PxJFnP1-_DyuUqhlBsPnj9Yy5b3abZg";
   var enableMessage = true;
   var enableEmoji = true;
   var arrayEmojis = [];
